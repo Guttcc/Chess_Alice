0b21a017eae3b04b8395851d34e32755b91cbe55
Guttcc <gutierrezcc8@gmail.com>
2026-07-22 12:21:47 +0200

    Script de las piezas
    Configuración de piezas y coordenadas

---
+++ Assets/Scripts/Chessman.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class Chessman : MonoBehaviour
+{
+    // References
+    public GameObject controller;
+    public GameObject movePlate;
+
+    // Positions
+    private int xBoard = -1;
+    private int yBoard = -1;
+
+    // Variable to keep track of "Black " or "White" pieces
+    private string player;
+
+    // Referemces fofr all the sprites that the chesspiece can be
+    public Sprite black_queen, black_king, black_bishop, black_knight, black_rook, black_pawn;
+    public Sprite white_queen, white_king, white_bishop, white_knight, white_rook, white_pawn;
+
+    public void Activate()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        // Take the instantiated location and adjust the transform
+        SetCoords();
+
+        switch (this.name)
+        {
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+
+
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+        }
+    }
+
+    public void SetCoords()
+    {
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
+    }
+}
