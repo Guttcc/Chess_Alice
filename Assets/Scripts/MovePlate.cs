2b1c3107bd70512bd2039d550e9fa6c10db8179b
Guttcc <173308154+Guttcc@users.noreply.github.com>
2026-07-23 00:01:46 +0200

    implemento de lógica de movimiento de las piezas
    Manejo de eventos del ratón para piezas, cálculo de trayectorias e instanciación de indicadores

---
+++ Assets/Scripts/MovePlate.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public GameObject controller;
+
+    GameObject reference = null;
+
+    //Board Positions, not worl positions
+    int matrixX;
+    int matrixY;
+
+    // False: movement, true: attacking
+    public bool attacked = false;
+
+
+    public void Start()
+    {
+        if (attacked)
+        {
+            // Change to red
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f);
+        }
+    }
+
+    public void OnMouseUp()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        if (attack)
+        {
+            GameObject cp = controller.GetComponent<Game>().GetPosition(matrixX, matrixY);
+
+            Destroy(cp);
+        }
+
+        controller.GetComponent<Game>().SetPositionEmpty(reference.GetComponent<Chessman>().GetXBoard(),
+            reference.GetComponent<Chessman>().GetYBoard());
+
+        reference.GetComponent<Chessman>().SetXBoard(matrixX);
+        reference.GetComponent<Chessman>().SetYBoard(matrixY);
+        reference.GetComponent<Chessman>().SetCoords();
+
+        controller.GetComponent<Game>().SetPosition(reference);
+
+        reference.GetComponent<Chessman>().DestroyMovePlates();
+    }
+
+    public void SetCoords(int x, int y)
+    {
+        matrixX = x;
+        matrixY = y;
+    }
+
+    public void SetReference(GameObject obj)
+    {
+        reference = obj;
+    }
+
+    public GameObject GetReference()
+    {
+        return reference;
+    }
+}
